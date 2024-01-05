2300d5f6035ab4f80dce225a7ee915185c1b871c
maryluzmeva <maryluzmeva@gmail.com>
2024-01-05 13:46:05 +0100

    CSS Media Queries set for Mobile Devices

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
     new Typewriter("#poem", {
       strings: response.data.answer,
       autoStart: true,
-      delay: natural,
+      delay: 'natural',
     });
   }
   
