16ae080bdd67bb8609ad870d5d4012cc7464d6d0
maryluzmeva <maryluzmeva@gmail.com>
2024-01-05 20:39:46 +0100

    Change the context for better answers

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function displayPoem(response) {
   
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "c71f439f65td859373faeeba102o0222";
-    let context = "You are a Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let context = "You are a Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. After the poem, Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
     let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
